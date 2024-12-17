9cf909d23474ea18129fb2e4bfad28e57b5175c1
Drakez777 <louisluzhang1997@gmail.com>
2024-12-17 12:58:05 -0800

    for ProductService CRUD, addProduct

--- ProductService/src/main/java/net/nvsoftware/ProductService/service/ProductServiceImpl.java
+++ ProductService/src/main/java/net/nvsoftware/ProductService/service/ProductServiceImpl.java
@@ -1,8 +1,28 @@
 package net.nvsoftware.ProductService.service;
 
+import net.nvsoftware.ProductService.entity.ProductEntity;
+import net.nvsoftware.ProductService.model.ProductRequest;
+import net.nvsoftware.ProductService.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Service
 public class ProductServiceImpl implements ProductService {
 
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public long addProduct(@RequestBody ProductRequest productRequest) {
+        ProductEntity productEntity = ProductEntity.builder()
+                .name(productRequest.getName())
+                .price(productRequest.getPrice())
+                .quantity(productRequest.getQuantity())
+                .build();
+
+        productRepository.save(productEntity);
+        return productEntity.getId();
+    }
 }
