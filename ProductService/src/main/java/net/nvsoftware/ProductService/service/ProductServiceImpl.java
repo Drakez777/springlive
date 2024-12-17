bce21e763555d47e3a47f6fa431f81a157a1b4a6
Drakez777 <louisluzhang1997@gmail.com>
2024-12-17 15:44:16 -0800

    intro log4j2 to enable local log

--- ProductService/src/main/java/net/nvsoftware/ProductService/service/ProductServiceImpl.java
+++ ProductService/src/main/java/net/nvsoftware/ProductService/service/ProductServiceImpl.java
@@ -1,5 +1,7 @@
 package net.nvsoftware.ProductService.service;
 
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
 import net.nvsoftware.ProductService.entity.ProductEntity;
 import net.nvsoftware.ProductService.model.ProductRequest;
 import net.nvsoftware.ProductService.model.ProductResponse;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Service
+@Log4j2
 public class ProductServiceImpl implements ProductService {
 
     @Autowired
@@ -18,6 +21,7 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public long addProduct(@RequestBody ProductRequest productRequest) {
+        log.info("Start: ProductService addProduct");
         ProductEntity productEntity = ProductEntity.builder()
                 .name(productRequest.getName())
                 .price(productRequest.getPrice())
@@ -25,15 +29,18 @@ public class ProductServiceImpl implements ProductService {
                 .build();
 
         productRepository.save(productEntity);
+        log.info("End: ProductService addProduct");
         return productEntity.getId();
     }
 
     @Override
     public ProductResponse getProductById(long id) {
+        log.info("Start: ProductService getProductById");
         ProductEntity productEntity = productRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("ProductService getById not found with id: " + id));
         ProductResponse productResponse = new ProductResponse();
         BeanUtils.copyProperties(productEntity, productResponse);
+        log.info("End: ProductService getProductById"+ productResponse);
         return productResponse;
     }
 }
