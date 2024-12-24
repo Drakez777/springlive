e3231d2479f4a4b3debf8b2017c446d3c20c8091
Drakez777 <louisluzhang1997@gmail.com>
2024-12-23 22:55:29 -0800

    setup openFeign and config to do inter microservice call by RPC not REST ip+port, change ProductService to legal name by openFeign

--- OrderService/src/main/java/net/nvsoftware/OrderService/service/OrderServiceImpl.java
+++ OrderService/src/main/java/net/nvsoftware/OrderService/service/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package net.nvsoftware.OrderService.service;
 
 import lombok.extern.log4j.Log4j2;
+import net.nvsoftware.OrderService.client.ProductServiceFeignClient;
 import net.nvsoftware.OrderService.entity.OrderEntity;
 import net.nvsoftware.OrderService.model.OrderRequest;
 import net.nvsoftware.OrderService.repository.OrderRepository;
@@ -15,6 +16,8 @@ public class OrderServiceImpl implements OrderService {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private ProductServiceFeignClient productServiceFeignClient;
 
     @Override
     public long placeOrder(OrderRequest orderRequest) {
@@ -30,8 +33,9 @@ public class OrderServiceImpl implements OrderService {
         orderRepository.save(orderEntity);
         log.info("Process: OderService placeOrder save orderEntity with orderId: " + orderEntity.getId()) ;
         // call ProductService to check product quantity, if ok, reduce it, else throw not enough
+        productServiceFeignClient.reduceQuantity(orderEntity.getProductId(), orderEntity.getQuantity());
         // call PaymentService to charge, if Success, mark orderPAID, else CANCELLED
-        log.info("End: OrderService placeOrder");
+        log.info("End: OrderService placeOrder FeignCall ProductService save orderIdentity with orderId: " + orderEntity.getId()) ;
         return orderEntity.getId();
     }
 }
