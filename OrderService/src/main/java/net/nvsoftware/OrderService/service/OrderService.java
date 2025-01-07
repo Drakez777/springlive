89da1bf1dc1660cdb71a4deff16ec3576d071c31
Drakez777 <louisluzhang1997@gmail.com>
2025-01-06 23:14:46 -0800

    getOrderDetailById

--- OrderService/src/main/java/net/nvsoftware/OrderService/service/OrderService.java
+++ OrderService/src/main/java/net/nvsoftware/OrderService/service/OrderService.java
@@ -2,9 +2,12 @@ package net.nvsoftware.OrderService.service;
 
 
 import net.nvsoftware.OrderService.model.OrderRequest;
+import net.nvsoftware.OrderService.model.OrderResponse;
 import org.springframework.stereotype.Service;
 
 
 public interface OrderService {
     long placeOrder(OrderRequest orderRequest);
+
+    OrderResponse getOrderDetailById(long orderId);
 }
